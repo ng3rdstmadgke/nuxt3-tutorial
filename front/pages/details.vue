7877b45db0e69baecd8b4b70c6ade1f10032b25f
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2023-05-04 14:56:37 +0900

    ひとまず動くところまで

---
+++ front/pages/details.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <header>Nuxt3サンプルアプリケーション</header>
+    <div class="container">
+      <article v-if="article">
+        <p>タイトル: {{ article.title }}</p>
+        <hr>
+        <div style="width: 500px">
+          {{ article.content }}
+        </div>
+      </article>
+      <NuxtLink to="/">戻る</NuxtLink>
+    </div>
+    <footer>2022</footer>
+  </div>
+</template>
+
+<style scoped>
+header {
+  border-bottom-style: solid;
+  padding: 1rem;
+}
+footer {
+  margin-top: 2rem;
+  background-color: #8080ee;
+  padding: 1rem;
+}
+.container {
+  margin: 2rem;
+}
+</style>
+
+<script setup lang="ts">
+const route = useRoute();
+const {id} = route.query;
+const articles = [
+  {
+    id: 1,
+    title: 'Nuxt3入門',
+    content: "Nuxt3が公式リリースされました。Nuxt3ではVue3対応だけでなく、NitroやVite等様々な改善が施されています。"
+  },
+  {
+    id: 2,
+    title: 'Jest再入門',
+    content: "今回はJestのモックについて整理していきます。Jestはビルトインでマッチャーが提供され、これ単体で多くのユースケースをサポートします。"
+  }
+];
+var article = ref<{id: number, title: string, content: string}>(null);
+article.value = articles.find(article => +id == article.id)
+
+</script>
---
+++ front/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header>Nuxt3サンプルアプリケーション</header>
+    <div class="container">
+      <p>新着記事</p>
+      <ul>
+        <li v-for="article in articles" :key="article.id">
+          <NuxtLink :to="{path: '/details', query: {id: article.id}}">
+            {{ article.title }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <footer>
+      2022
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const articles = ref<{ id: number; title: string }[]>([]);
+articles.value = [
+  {
+    id: 1,
+    title: 'Nuxt3入門'
+  },
+  {
+    id: 2,
+    title: 'Jest再入門' 
+  }
+]
+</script>
+
+<style scoped>
+header {
+  border-bottom-style: solid;
+  padding: 1rem;
+}
+footer {
+  margin-top: 2rem;
+  background-color: #8080ee;
+  padding: 1rem;
+}
+.container {
+  margin: 2rem;
+}
+</style>
